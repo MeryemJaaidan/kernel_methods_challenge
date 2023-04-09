903b249ea28ff066be5408f4f79aa797d0b190c9
wenjiaa <94172818+wenjiaa@users.noreply.github.com>
2023-04-09 16:45:21 +0200

    Add files via upload

--- src/kernels.py
+++ src/kernels.py
@@ -2,10 +2,16 @@ import numpy as np
 import networkx as nx
 from scipy import sparse
 
-def K_n_walk(G1, G2, n=2):
-    G = nx.tensor_product(G1, G2)
-    A = nx.adjacency_matrix(G)
-    K = (A ** n).sum()
+def K_n_walk(data_set, n=2):
+    d = len(data_set)
+    K = np.zeros((d,d))
+    for i in range(d):
+        for j in range(i+1):
+            G = nx.tensor_product(data_set[i], data_set[j])
+            A = nx.adjacency_matrix(G)
+            K[i,j] = (A ** n).sum()
+            K[j,i] = K[i,j]
+    
     return K
 
 # def K_random_walk(G1, G2):
